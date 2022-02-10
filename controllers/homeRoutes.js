0f94bda248d51b26a19054993ba5da9ec90a8cff
John Climie <johnclimie00@gmail.com>
2022-02-10 14:18:13 -0500

    Navbar links go to internal according titles

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,22 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     try {
+        res.render('homepage');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/login', async (req, res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/dashboard', async (req, res) => {
+    try {
         res.render('dashboard');
     } catch (err) {
         res.status(500).json(err);
