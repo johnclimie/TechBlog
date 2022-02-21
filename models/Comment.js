2a915674927ab4db5f9530f0092b2f03830f6712
John Climie <johnclimie00@gmail.com>
2022-02-21 13:32:06 -0500

    Created more models for posts and comments

---
+++ models/Comment.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model{}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        commentcontent: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }, 
+
+        date: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+
+    {
+        sequelize,
+        timestamps:false,
+        freezeTableName:true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment;
---
+++ models/Post.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model{}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }, 
+        postcontent: {
+            type:DataTypes.STRING,
+            allowNull: false
+        },
+        date: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+
+    {
+        sequelize,
+        timestamps:false,
+        freezeTableName:true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
