97dbe09f8590c6ed3fac12c92489e94649043183
John Climie <johnclimie00@gmail.com>
2022-02-22 15:55:52 -0500

    Created login and signup functions

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,20 @@ const loginFormHandler = async (event) => {
 
     const email = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'applicaiton/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in.')
+        }
+    }
 }
 
 
@@ -12,4 +26,29 @@ const signupFormHandler = async (event) => {
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#password-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/users/', {
+            method: 'POST',
+            body: JSON.stringify({
+                username, email, password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            console.log('success');
+
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
 }
+
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
+
