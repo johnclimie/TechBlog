72549918e65e3d7a602c1220d419983b1e31b495
John Climie <johnclimie00@gmail.com>
2022-02-22 16:36:56 -0500

    Added login/signup functionality

--- public/js/login.js
+++ public/js/login.js
@@ -24,7 +24,7 @@ const signupFormHandler = async (event) => {
     event.preventDefault()
 
     const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#password-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
     if (username && email && password) {
