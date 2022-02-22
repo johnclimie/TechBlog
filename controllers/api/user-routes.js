d895bfb10c53dfca8ea8531677072520185222ec
John Climie <johnclimie00@gmail.com>
2022-02-22 16:08:32 -0500

    Created user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,60 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.post('/', async (req, res) => {
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try {
+
+        const dbUserData = await User.findOne({
+            where: {
+                email: req.body.email,
+            }
+        });
+
+        if (!dbUserData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password. Please try again!' });
+                return;
+        }
+
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password. Please try again!' });
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.loggedIn = true;
+
+            res
+                .status(200)
+                .json({ user: dbUserData, message: 'You are now logged in! '});
+            return;
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
