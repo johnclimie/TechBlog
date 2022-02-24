f7e877cc5c5bbd1201e41c26675b20ada81b55bf
John Climie <johnclimie00@gmail.com>
2022-02-24 14:21:03 -0500

    Added more post route improvements

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,8 +3,20 @@ const { Post } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
+        const date = new Date();
+        const dateString = date.toLocaleDateString('en-us', { year:'numeric', month: 'long', day:'numeric' })
 
+        const dbUserData = await Post.create({
+            username: req.session.username,
+            postcontent: req.body.postcontent,
+            date: dateString
+        });
+
+        res.status(200).json(dbUserData);
     } catch (err) {
-        
+        console.log(err);
+        res.status(500).json(err);
     }
 })
+
+module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -11,7 +11,7 @@ router.post('/', async (req, res) => {
 
         req.session.save(() => {
             req.session.loggedIn = true;
-
+            req.session.username = dbUserData.username;
             res.status(200).json(dbUserData);
         });
     } catch (err) {
