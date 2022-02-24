e7081275ba6268b406a2ee11725b8d68bd8ac7c4
John Climie <johnclimie00@gmail.com>
2022-02-24 18:34:43 -0500

    Set up add comment function

---
+++ public/js/addcomment.js
@@ -0,0 +1,27 @@
+async function addCommentFormHandler(event) {
+    event.preventDefault();
+
+    const commentcontent = document.querySelector('#commentcontent').value.trim();
+
+    if (commentcontent) {
+        const response = await fetch('#', {
+            method: 'POST',
+            body: JSON.stringify({
+                commentcontent
+            }),
+            headers: {
+                'content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            console.log('success');
+            
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText)
+        }
+    }
+}
+
+document.querySelector('#addcomment')
