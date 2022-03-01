03741e6973e535f06f27986e9af4d63bb83158d3
John Climie <johnclimie00@gmail.com>
2022-02-28 19:32:46 -0500

    Edit post maitnence

--- controllers/api/edit-routes.js
+++ controllers/api/edit-routes.js
@@ -1,5 +1,24 @@
 const router = require('express').Router();
 const { Post } = require('../../models'); 
 
+router.put('/', async (req, res) => {
+    try {
+        const date = new Date();
+        const dateString = date.toLocaleDateString('en-us', { year:'numeric', month: 'long', day:'numeric' });
+        console.log(dateString);
+
+        const dbPostData = await Post.update({
+            title: req.body.title,
+            postcontent: req.body.postcontent
+        });
+
+        await Post.save();
+
+        res.status(200).json(dbPostData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
